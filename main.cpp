dc755ac2fda278b28bca7bc31682325f050e5ea6
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-06-25 12:21:36 -0700

    first program- Ask user for number

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/***************************
+* Kobe Shelby
+
+* main.cpp
+
+* 06.25.2024
+
+****************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int favorite_number; // This is a variable favorite number is stored
+
+    cout << "Enter your favorite number between 1 and 100: ";
+
+    cin >> favorite_number;
+
+    cout << "Amazing!! That's my favorite number too!" << endl;
+
+    cout << "No really!!, " << favorite_number << " is my favoite number!";
+
+    return 0; 
+}
