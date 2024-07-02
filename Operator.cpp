0221418a62c41c09bef8e884c42da945fa6f1f8a
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-07-01 20:35:07 -0700

    adding to the array

--- Operator.cpp
+++ Operator.cpp
@@ -3,13 +3,34 @@ using namespace std;
 
 int main() {
 
-    const int num1 = 10;
-    int num2 = 20;
+    // const int num1 = 10;
+    // int num2 = 20;
 
-    num1 = 100;
+    // num1 = 100;
 
-    cout << "num1 is " << num1 << endl;
-    cout << "num2 is " << num2 << endl;
+    // cout << "num1 is " << num1 << endl;
+    // cout << "num2 is " << num2 << endl;
+
+    int num1 = 200;
+    int num2 = 100; 
+    int result = 0;
+    
+    result = num1 + num2;
+    cout << num1 << " + " << num2 << " = " << result << endl;
+
+    result = num1 - num2;
+    cout << num1 << " - " << num2 << " = " << result << endl;
+
+    result = num1 * num2;
+    cout << num1 << " * " << num2 << " = " << result << endl;
+    
+    result = num1 / num2;
+    cout << num1 << " / " << num2 << " = " << result << endl;
+
+    result = num1 % num2;
+    cout << num1 << " % " << num2 << " = " << result << endl;
+
+    
 
     return 0;
 
--- dimensional_array.cpp
+++ dimensional_array.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 int main() {
-    const int rows = 3;
-    const int cols = 4;
-    int movie_ratings [rows] [cols];
 
-    
+    const int row = 6;
+    const int cols = 5;
+
+    int movie_ratings[row][cols] = {
+        {3, 4, 5, 6, 54},
+        {7, 8, 9, 10, 89},
+        {11, 12, 13, 14, 74},
+        {15,16,17,18, 92},
+        {19,20,21,22, 100},
+        {23,24,25,26, 150},
+
+    };
+
+
+
+    cout << "This is 2D array: " << movie_ratings[5][4];
+
 
     return 0;
 
