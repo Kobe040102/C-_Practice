e9d85faf6c8b3344a56802e1440c285e2d8d5e66
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-07-06 16:11:53 -0700

    if-nested

--- Basics/if_statement.cpp
+++ Basics/if_statement.cpp
@@ -10,13 +10,13 @@ int main() {
     cout << "Enter a number between " << min << " and " << max << ":";
     cin >> num;
 
-    if (num >= min) {
-        cout << "\n======= if statement 1 =================" << endl;
-        cout << num << " is greater than " << min << endl;
+    // if (num >= min) {
+    //     cout << "\n======= if statement 1 =================" << endl;
+    //     cout << num << " is greater than " << min << endl;
 
-        int diff = (num - min);
-        cout << num << " is " << diff << " greater than " << min << endl;
-    }
+    //     int diff = (num - min);
+    //     cout << num << " is " << diff << " greater than " << min << endl;
+    // }
 
     if (num <= max) {
         cout << "\n======= if statement 2 =================" << endl;
@@ -27,16 +27,16 @@ int main() {
     
     }
 
-    if (num >= min && num <= max) {
-        cout << "\n======= if statement 3 =================" << endl;
-        cout << num << "is in range "<< endl;
-        cout << "This means statement 1 and 2 must also display!!" << endl; 
-    }
+    // if (num >= min && num <= max) {
+    //     cout << "\n======= if statement 3 =================" << endl;
+    //     cout << num << "is in range "<< endl;
+    //     cout << "This means statement 1 and 2 must also display!!" << endl; 
+    // }
 
-    if(num == min || num ==max) {
-        cout << "\n======= if statement 3 =================" << endl;
-        cout << num << "is right on a boundry" << endl;
-        cout << "This means all 4 statements display " << endl;
-    }
+    // if(num == min || num ==max) {
+    //     cout << "\n======= if statement 3 =================" << endl;
+    //     cout << num << "is right on a boundry" << endl;
+    //     cout << "This means all 4 statements display " << endl;
+    // }
     return 0;
 }
