29cbe43f5080d84b047d4a89ba3d004524e17c64
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-07-16 13:06:22 -0700

    This is a test range-loop

--- Basics/for-loop.cpp
+++ Basics/for-loop.cpp
@@ -63,18 +63,32 @@ int main() {
     // for(auto score : scores) 
     // cout << score << endl;
 
-    vector<double> temperatures = {87.9, 77.9, 80.0, 72.5};
-    double average_temp;
-    double total;
+    // vector<double> temperatures = {87.9, 77.9, 80.0, 72.5};
+    // double average_temp;
+    // double total;
 
-    for(auto temp:temperatures)
-        total += temp;
+    // for(auto temp:temperatures)
+    //     total += temp;
     
-    if(temperatures.size() != 0)
-        average_temp = total / temperatures.size();
+    // if(temperatures.size() != 0)
+    //     average_temp = total / temperatures.size();
 
-        cout << fixed << setprecision(1);
+    //     cout << fixed << setprecision(1);
 
-        cout << "Average temperature is " << average_temp << endl;;
+    //     cout << "Average temperature is " << average_temp << endl;
+
+    // for(auto val:{1,2,3,4,5})
+    //     cout << val << endl;
+    //     cout << endl;
+
+    for(auto c: "This is a test") {
+        if(c == ' ') {
+            cout << "\n";
+           } else {
+            cout << c;
+            }
+        }
+    cout << endl;
     return 0;
+   
 }
