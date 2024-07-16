aa99d1e6057a0ce093774bf78f7f6933915e54b2
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-07-16 13:53:27 -0700

    more while-loop practice

--- Basics/while-loop.cpp
+++ Basics/while-loop.cpp
@@ -18,11 +18,68 @@ int main() {
     //     i++;
     // }
 
-    int scores [] = {100, 90, 87};
-    int i = 2;
+    // int scores [] = {100, 90, 87};
+    // int i = 2;
 
-    while (i < 3){
-        cout << scores[i] << endl;
-        i++;
+    // while (i < 3){
+    //     cout << scores[i] << endl;
+    //     i++;
+    // }
+
+    // Asking user to input a integer that is less than 100
+//     int number;
+
+//     cout << "Enter a integer less than 100: ";
+//     cin >> number;
+
+//     while(number >= 100){
+//         cout << "please try again: ";
+//         cin >> number; 
+//     } 
+//     cout << "Thanks!" << endl;
+
+// }
+
+// Asking user between 1 and 5
+// int num;
+
+// cout << "Please enter a number between 1 and 5: ";
+// cin >> num;
+
+// while( num <= 1 || num >= 5){
+//     cout << "Please follow instructions: ";
+//     cin >> num;
+// }
+
+// cout << "You number is " << num;
+// }
+
+// input validation = boolean flag
+// bool done = false;
+// int number = 0;
+
+// while(!done) {
+//     cout << "Enter an integer between 1 and 5: ";
+//     cin >> number;
+//     if(number <= 1 || number >= 5){
+//         cout << "Out of range, try again" << endl;
+//         } else {
+//             cout << "Thanks!" << endl;
+//             done = true;
+//         }
+//     }
+
+// Blast off program
+int num;
+    cout << "Enter a positive integer - start the countdown: ";
+    cin >> num;
+
+    while(num > 0){
+        cout << num << endl;
+        num--;
     }
+    cout << "BlastOff!";
+
 }
+
+
