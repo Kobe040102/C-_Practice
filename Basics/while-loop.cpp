d3d9ba0cb02cb6be88e4ca09c5b8b604b6a67e2e
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-07-16 13:17:47 -0700

    while-loop practice

---
+++ Basics/while-loop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int i = 1;
+
+    // while(i <=5) {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // int i = 1;
+
+    // while(i <= 10) {
+    //     if(i % 5 == 0){
+    //         cout << i << endl;
+    //     }
+    //     i++;
+    // }
+
+    int scores [] = {100, 90, 87};
+    int i = 2;
+
+    while (i < 3){
+        cout << scores[i] << endl;
+        i++;
+    }
+}
