e655ff82cba2379be28cf7564732658f9e40b2bf
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-08-03 22:40:33 -0700

    finding the even number in vector

--- Basics/do-while.cpp
+++ Basics/do-while.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main(){
@@ -20,25 +21,49 @@ int main(){
     // } while(selection == 'N' || selection == 'n'); 
     //     cout << "Thank you! Your name is " <<  name << " and your age is " << age;
     
-    char selection;
-    do {
-        cout << "\n---------------------" << endl;
-        cout << "1.Do this" << endl;
-        cout << "2.Do that" << endl;
-        cout << "3.Do something else" << endl;
-        cout << "Q.Quit" << endl;
-        cout << "\nEnter your selection: ";
-        cin >> selection;
+    // char selection;
+    // do {
+    //     cout << "\n---------------------" << endl;
+    //     cout << "1.Do this" << endl;
+    //     cout << "2.Do that" << endl;
+    //     cout << "3.Do something else" << endl;
+    //     cout << "Q.Quit" << endl;
+    //     cout << "\nEnter your selection: ";
+    //     cin >> selection;
 
-        if(selection == '1') {
-            cout << "\nDo this";
-        } else if(selection == '2') {
-            cout << "\nDo that";
-        } else if(selection == '3') {
-            cout << "\nDo something else";
-        }
+    //     if(selection == '1') {
+    //         cout << "\nDo this";
+    //     } else if(selection == '2') {
+    //         cout << "\nDo that";
+    //     } else if(selection == '3') {
+    //         cout << "\nDo something else";
+    //     } else if(selection == 'Q' || selection == 'q') {
+    //         cout << "goodbye!" << endl;
+    //     } else {
+    //         cout << "Unknown option --try again!";
+    //     }
         
-    } while(selection != 'q' && selection != 'Q');
+    // } while(selection != 'q' && selection != 'Q');
+
+    vector<int> nums = { 13, 79, 97, 23, 61, 46 };
+    int index = 0;
+    bool found = false;
+
+    do {
+         int it = nums[index];
+         if(it % 2 == 0) {
+             cout << "Even found: " << it;
+             found = true;
+         } else {
+             index++;
+         }
+
+
+    } while(index < nums.size() && !found);
+
+    if(!found) {
+        cout << "No even numbers found";
+    }
 
     return 0;
 }
