c04a5abd99b97cf30a341cf46dda49ebb2faad0d
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-08-09 14:10:00 -0700

    inifite-loop

--- Basics/infinite-loop.cpp
+++ Basics/infinite-loop.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    while(true) {
-        char again;
-        cout << "Do you want to loop again? (Y/N): ";
-        cin >> again;
+    // while(true) {
+    //     char again;
+    //     cout << "Do you want to loop again? (Y/N): ";
+    //     cin >> again;
 
-        if(again == 'N' || again == 'n')
-            break;
+    //     if(again == 'N' || again == 'n')
+    //         break;
+    // }
+
+    // Display the 2d vector
+    vector<vector<int>> vector_2d = {
+        {1,2,3},
+        {10,20,30,40},
+        {100,200,300,400,500}
+    };
+
+    for (auto vec: vector_2d){
+        for(auto val: vec) {
+            cout << val << " ";
+        }
+        cout << endl;
     }
+
+    //Multiplcation table 
+
+    
 }
