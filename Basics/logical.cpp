3877fd2cba265f47509474e73c73517bb14ae7fe
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-08-30 21:45:51 -0700

    logical operators

--- Basics/logical.cpp
+++ Basics/logical.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     
-    int num;
-    const int lower = 10;
-    const int upper = 20;
+    // int num;
+    // const int lower = 10;
+    // const int upper = 20;
 
     // cout << "Enter an integer - the bounds are " << lower << " and " << upper << ":";
     // cin >> num;
@@ -31,27 +31,69 @@ int main() {
     // on_bounds=(num == lower || num == upper);
     // cout << num << "is on one of the bounds which are "<< lower << " and " << upper << " : " << on_bounds << endl;
 
-    bool wear_coat = false;
-    double temperature;
-    int wind_speed;
+    // bool wear_coat = false;
+    // double temperature;
+    // int wind_speed;
 
-    const int wind_speed_for_coat = 25;
-    const double temperature_for_coat = 45;
+    // const int wind_speed_for_coat = 25;
+    // const double temperature_for_coat = 45;
 
-    cout << "\nEnter the current temperature in (F):";
-    cin >> temperature;
-    cout << "Enter windspeed in (mph):";
-    cin >> wind_speed;
+    // cout << "\nEnter the current temperature in (F):";
+    // cin >> temperature;
+    // cout << "Enter windspeed in (mph):";
+    // cin >> wind_speed;
 
-    wear_coat = (temperature < temperature_for_coat || wind_speed > wind_speed_for_coat);
-    cout << "Do you need to wear a coat using OR? " << wear_coat << endl;
+    // wear_coat = (temperature < temperature_for_coat || wind_speed > wind_speed_for_coat);
+    // cout << "Do you need to wear a coat using OR? " << wear_coat << endl;
 
-    wear_coat = (temperature < temperature_for_coat&&wind_speed > wind_speed_for_coat);
-    cout << "Do you need to wear a coat using AND? " << wear_coat << endl;
+    // wear_coat = (temperature < temperature_for_coat&&wind_speed > wind_speed_for_coat);
+    // cout << "Do you need to wear a coat using AND? " << wear_coat << endl;
 
+    // cout << endl;
+
+    // Checks if out of range 
+    // int num;
+    // bool range;
+
+    // cout << "Please input a number between 10 and 20: ";
+
+    // cin >> num;
+
+    // if(num >= 10 && num <= 20) {
+    //     range = true;
+    //     cout << "true! Number is in range.";
+    // } else {
+    //     range = false;
+    //     cout << "false! Number is out of range.";
+    // }
+
+    // Check for multiple conditions
+    bool discount;
+    char student;
+    char senior;
+    int purchase;
+
+    cout << "Are you a student? ";
     
+    cin >> student;
 
-    cout << endl;
+    if(student == 'Y'){
+        cout << "You are elgiable for a discount!";
+        discount = true;
+    } else if(student == 'N'){
+        cout << "That's fine! Are you a senior citizen and have you made a purchase of $50 or more? ";
+        cin >> senior;
+        cin >> purchase;
+    } else if(senior == 'Y' && purchase >= 50) {
+        cout << "You are elgiable for a discount!";
+        discount = true;
+    } else if(senior == 'N') {
+        cout << "Sorry you do not qualify for the discount";
+        discount = false;
+    } else {
+        cout << "Does not compute. ERROR!";
+    }
+
     return 0;
 
 }
