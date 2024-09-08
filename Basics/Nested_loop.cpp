c7f14aaba89526f9cdd29a619f30f6be3b14b21d
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-09-08 13:36:52 -0700

    nested loop practice

---
+++ Basics/Nested_loop.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // // Outer loop prints each number the amount of times of the inner val 
+    //  for(int outer_val = 1; outer_val <= 10; outer_val++) {
+    // // Inner loop prints the numbers consecutively
+    //      for(int inner_val = 1; inner_val <= 10; inner_val++) {
+    //          cout << outer_val << ", " << inner_val << endl;
+    //      }
+    //  }
+
+    // for(int num1 = 1; num1 <= 10; num1++){
+    //     for(int num2 = 1; num2 <= 10; num2++ ) {
+    //         cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    //     }
+    //     cout << "------------------" << endl;
+    // }
+
+    int grid[5][3] = {{1,2,3},
+                    {4,5,6},
+                    {7,8,9},
+                    {10,11,12},
+                    {13,14,15}};
+
+    for(int row = 0; row < 5; row++){
+        for(int col = 0; col < 3; col++){
+            cout << grid[row][col] << ", ";
+        }
+        cout << endl;
+    }
+}
