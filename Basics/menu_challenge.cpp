b0b95c7c7811ce08016c20e5958722ca1c47141f
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-09-15 19:57:25 -0700

    menu challenge finding the Mean

--- Basics/menu_challenge.cpp
+++ Basics/menu_challenge.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main() {
     vector<int> data;
     char selection;
+    int addNum;
+    double sum = 0;
+    double mean = 0;
     do {
+    cout << " " << endl;
+    cout << "--------------------------------------" << endl;
     cout << "Please choose from the following menu!" << endl;
     cout << "Menu options: " << endl;
     cout << "P - Print numbers" << endl;
@@ -19,7 +24,28 @@ int main() {
     cin >> selection ;
 
     if(selection == 'P' || selection == 'p'){
-
+        if(data.empty()){
+            cout << "[] - the list is empty";
+        } else {
+            for(int i = 0; i < data.size(); i++){
+            cout << " " << data[i];
+        }
+    }
+    } else if(selection == 'A' || selection == 'a'){
+         cout << "Please add a number to the list: ";
+         cin >> addNum;
+         data.push_back(addNum);
+         cout << "Number has been added!" << endl;
+    } else if(selection == 'M' || selection == 'm'){
+        if(data.empty()) {
+            cout << "Unable to calulate the mean - no data";
+        } else {
+         for(int i = 0; i < data.size(); i++){
+             sum = sum += data[i];
+             mean = sum / static_cast<double>(data.size());
+         }
+         cout << "The mean is: " << mean;
+        }
     }
     
     } while(selection != 'Q' && selection != 'q');
