65c437f515d7ace23c023db40433f8b75aea84ea
Kobe Shelby <Kobeshelby04@gmail.ccom>
2024-09-15 17:25:17 -0700

    update on the menu challnege

--- Basics/menu_challenge.cpp
+++ Basics/menu_challenge.cpp
@@ -4,17 +4,28 @@ using namespace std;
 // Menu challenge
 int main() {
     vector<int> data;
-    char answ;
+    char selection;
+    do {
     cout << "Please choose from the following menu!" << endl;
     cout << "Menu options: " << endl;
-    cout << "P - Print number" << endl;
+    cout << "P - Print numbers" << endl;
     cout << "A - Add a number" << endl;
+    cout << "M - Display mean of the numbers" << endl;
     cout << "S - Display the smallest number" << endl;
     cout << "L - Display the largest" << endl;
     cout << "Q - Quit" << endl;
-
+    
     cout << "Enter your choice: ";
-    cin >> answ;
+    cin >> selection ;
+
+    if(selection == 'P' || selection == 'p'){
+
+    }
+    
+    } while(selection != 'Q' && selection != 'q');
+    cout << "You quit the program. Goodbye!";
+
+    
 
 
 
